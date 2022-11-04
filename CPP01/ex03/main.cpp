ba4bc8bcded5d834e9421ad8e8aa7d4272aab58b
Maria Daan <mariadaan@gmail.com>
2022-11-04 15:45:23 +0100

    bestaande main gebruiken

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -2,20 +2,41 @@
 // #include "HumanA.hpp"
 #include "HumanB.hpp"
 
-int main(void)
+// int main(void)
+// {
+// 	std::cout << "------- Person B -------" << std::endl;
+// 	HumanB personB;
+// 	personB.setName("person B");
+// 	personB.attack();
+// 	Weapon *bWeapon = new Weapon();
+// 	personB.setWeapon(*bWeapon);
+// 	personB.attack();
+// 	delete bWeapon;
+
+// 	std::cout << std::endl << "------- Person A -------" << std::endl;
+
+
+
+// 	return (0);
+// }
+
+
+int main()
 {
-	std::cout << "------- Person B -------" << std::endl;
-	HumanB personB;
-	personB.setName("person B");
-	personB.attack();
-	Weapon *bWeapon = new Weapon();
-	personB.setWeapon(*bWeapon);
-	personB.attack();
-	delete bWeapon;
-
-	std::cout << std::endl << "------- Person A -------" << std::endl;
-
-
-
-	return (0);
+	// {
+	// 	Weapon club = Weapon("crude spiked club");
+	// 	HumanA bob("Bob", club);
+	// 	bob.attack();
+	// 	club.setType("some other type of club");
+	// 	bob.attack();
+	// }
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+return 0;
 }

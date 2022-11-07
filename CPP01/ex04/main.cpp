68bf33945b38c319607dabdbf84ed579b9211444
Maria Daan <mariadaan@gmail.com>
2022-11-07 15:31:17 +0100

    organise

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,42 +1,4 @@
-#include <fstream>
-#include <iostream>
-
-
-/* replace all occurences of s1 in line by s2 */
-void replaceLine(std::string &line, std::string &s1, std::string &s2)
-{
-	std::size_t s1_index = line.find(s1);
-	while (s1_index != -1 && s1_index!=std::string::npos)
-	{
-		line.erase(s1_index, s1.length());
-		line.insert(s1_index, s2);
-		s1_index = line.find(s1, s1_index + 1);
-	}
-}
-
-/* replace all occurences of s1 in file by s2 */
-int replaceFile(std::string &filename, std::string &s1, std::string &s2)
-{
-	std::string line;
-	std::ifstream ini_file(filename);
-	std::ofstream out_file(filename + ".replace");
-	if (ini_file && out_file)
-	{
-		while (getline(ini_file, line))
-		{
-			replaceLine(line, s1, s2);
-			out_file << line << std::endl;
-		}
-	}
-	else
-	{
-		std::cout << "Can not read file" << std::endl;
-		return (0);
-	}
-	ini_file.close();
-	out_file.close();
-	return (1);
-}
+#include "sed.hpp"
 
 int main(int argc, char **argv)
 {
---
+++ CPP01/ex04/sed.hpp
@@ -0,0 +1,5 @@
+#include <fstream>
+#include <iostream>
+
+void replaceLine(std::string &line, std::string &s1, std::string &s2);
+int replaceFile(std::string &filename, std::string &s1, std::string &s2);

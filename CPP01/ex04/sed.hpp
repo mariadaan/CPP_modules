28f03b3ab1252b81fbd13b1c6c20999ac13c799a
Maria Daan <mariadaan@gmail.com>
2022-11-07 15:41:50 +0100

    formatting

--- CPP01/ex04/sed.hpp
+++ CPP01/ex04/sed.hpp
@@ -1,5 +1,10 @@
+#ifndef SED_HPP
+#define SED_HPP
+
 #include <fstream>
 #include <iostream>
 
 void replaceLine(std::string &line, std::string &s1, std::string &s2);
 int replaceFile(std::string &filename, std::string &s1, std::string &s2);
+
+#endif

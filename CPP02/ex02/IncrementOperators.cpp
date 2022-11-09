0e7bcbf7b4ed9b7aa3eeab6ceb6269100dc3f7d3
Maria Daan <mariadaan@gmail.com>
2022-11-09 19:58:55 +0100

    remove comments

--- CPP02/ex02/IncrementOperators.cpp
+++ CPP02/ex02/IncrementOperators.cpp
@@ -1,13 +1,13 @@
 #include "Fixed.hpp"
 
-/* pre-increment */ //deze gaat goed
+/* pre-increment */
 Fixed &Fixed::operator++(void)
 {
 	this->setRawBits(this->getRawBits() + 1);
 	return (*this);
 }
 
-/* post-increment */ // deze NIET GOED
+/* post-increment */
 Fixed Fixed::operator++(int)
 {
 	Fixed copy(*this);

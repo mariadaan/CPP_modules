1108500d4741816becc51510d474725ae68d6304
Maria Daan <mariadaan@gmail.com>
2022-11-10 15:49:45 +0100

    claptrap

--- CPP03/ex01/ClapTrap.cpp
+++ CPP03/ex01/ClapTrap.cpp
@@ -1,9 +1,19 @@
 #include "ClapTrap.hpp"
 
+/* Default constructor */
+ClapTrap::ClapTrap(void)
+{
+	std::cout << "ClapTrap default constructed" << std::endl;
+	this->_name = "";
+	this->_hitPoints = 10;
+	this->_energyPoints = 10;
+	this->_attackDamage = 0;
+}
+
 /* Constructor */
 ClapTrap::ClapTrap(std::string name)
 {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << "ClapTrap constructed: " << name << std::endl;
 	this->_name = name;
 	this->_hitPoints = 10;
 	this->_energyPoints = 10;
@@ -13,14 +23,14 @@ ClapTrap::ClapTrap(std::string name)
 /* Copy constructor */
 ClapTrap::ClapTrap(ClapTrap &clapTrapper)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	std::cout << "ClapTrap copy constructor called" << std::endl;
 	*this = clapTrapper;
 }
 
 /* Copy assignment constructor */
 ClapTrap& ClapTrap::operator=(ClapTrap &clapTrapper)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
+	std::cout << "ClapTrap copy assignment operator copied " << clapTrapper.getName() << std::endl;
 	this->_name = clapTrapper.getName();
 	this->_hitPoints = clapTrapper.getHitPoints();
 	this->_energyPoints = clapTrapper.getEnergyPoints();
@@ -28,9 +38,10 @@ ClapTrap& ClapTrap::operator=(ClapTrap &clapTrapper)
 	return clapTrapper;
 }
 
+/* Destructor */
 ClapTrap::~ClapTrap(void)
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "ClapTrap destructed: " << this->_name << std::endl;
 }
 
 /*
@@ -44,12 +55,19 @@ void	ClapTrap::attack(const std::string &target)
 {
 	std::string str;
 
-	str = "ClapTrap <name> attacks <target>, causing <damage> points of damage.\n";
-	str.replace(str.find("<name>"), 6, this->_name);
-	str.replace(str.find("<target>"), 8, target);
-	str.replace(str.find("<damage>"), 8, std::to_string(this->_attackDamage));
-	std::cout << str;
-	this->_energyPoints--;
+	if (this->_energyPoints > 0)
+	{
+		str = "ClapTrap <name> attacks <target>, causing <damage> points of damage.\n";
+		str.replace(str.find("<name>"), 6, this->_name);
+		str.replace(str.find("<target>"), 8, target);
+		str.replace(str.find("<damage>"), 8, std::to_string(this->_attackDamage));
+		std::cout << str;
+		this->_energyPoints--;
+	}
+	else
+	{
+		std::cout << "Claptrap " << this->_name << " does not have enough energy points to attack";
+	}
 }
 
 /*
@@ -59,10 +77,14 @@ void	ClapTrap::takeDamage(unsigned int amount)
 {
 	std::string str;
 
+	this->_hitPoints -= amount;
+	if (this->_hitPoints < 0)
+		this->_hitPoints = 0;
+
 	str = "ClapTrap <name> takes damage, loses <amount> hitpoints.\n";
 	str.replace(str.find("<name>"), 6, this->_name);
 	str.replace(str.find("<amount>"), 8, std::to_string(amount));
-	this->_hitPoints -= amount;
+	std::cout << str;
 }
 
 /*
